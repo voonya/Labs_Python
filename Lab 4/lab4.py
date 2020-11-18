2315c5463bf8b10383fd054ba2ea914fdc298384
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2020-11-18 14:36:38 +0200

    add lab4

--- Lab 4/lab4.py
+++ Lab 4/lab4.py
@@ -1,8 +1,9 @@
+''' Варіант 12'''
 n = int(input("Input the limit of degrees: "))
 tC = 0
 tF = 0
-print("%6s %6s"%("tC","tF"))
-for i in range(0,n+1):
+print("%6s %6s" % ("tC", "tF"))
+for i in range(0, n+1):
     tC = i
     tF = 9 * tC / 5 + 32
-    print("%6d | %5.2f" %(tC,tF),sep="|")
+    print("%6d | %5.2f" % (tC, tF), sep="|")
